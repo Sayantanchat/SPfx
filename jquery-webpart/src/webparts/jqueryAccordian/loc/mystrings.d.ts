680b226c5b1f34822641fc8c0f5875487bf59fa4
sayantan <sayantan@me.com>
2018-03-25 21:43:11 +0000

    JQuery Accondian with test data

---
+++ jquery-webpart/src/webparts/jqueryAccordian/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJqueryAccordianWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JqueryAccordianWebPartStrings' {
+  const strings: IJqueryAccordianWebPartStrings;
+  export = strings;
+}
